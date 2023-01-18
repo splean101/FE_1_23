00c5618487f386c598174990ae6f98904ae0f8f9
splean101 <101splean@gmail.com>
2023-01-18 10:47:34 +0200

    third done

--- script.js
+++ script.js
@@ -14,37 +14,104 @@
 // console.log(sum2(arr));
 
 //2.
-function deepCount1(array) {
-  let sum = array.length;
-  for (const el of array) {
-    if (Array.isArray(el)) {
-      sum += deepCount1(el);
+// function deepCount1(array) {
+//   let sum = array.length;
+//   for (const el of array) {
+//     if (Array.isArray(el)) {
+//       sum += deepCount1(el);
+//     }
+//   }
+//   return sum;
+// }
+
+// function deepCount2(array) {
+//   let sum = array.length;
+//   for (const el of array) {
+//     Array.isArray(el) ? (sum += deepCount1(el)) : null;
+//   }
+//   return sum;
+// }
+
+// console.log(deepCount1([])); //0
+// console.log(deepCount1([1, 2, 3])); //3
+// console.log(deepCount1(['x', 'y', ['z']])); //4
+// console.log(deepCount1([1, 2, [3, 4, [5]]])); //7
+// console.log(deepCount1([[[[]]]])); //3
+
+// console.log(deepCount2([])); //0
+// console.log(deepCount2([1, 2, 3])); //3
+// console.log(deepCount2(['x', 'y', ['z']])); //4
+// console.log(deepCount2([1, 2, [3, 4, [5]]])); //7
+// console.log(deepCount2([[[[]]]])); //3
+//3.
+
+const employees = {
+  development: {
+    backend: [
+      { name: 'Mike', salary: 2000 },
+      { name: 'Nikola', salary: 2500 },
+    ],
+    frontend: [
+      { name: 'Artem', salary: 3000 },
+      { name: 'Alex', salary: 2700 },
+    ],
+  },
+  sales: {
+    marketing: {
+      internet_marketers: [
+        { name: 'Nina', salary: 1000 },
+        { name: 'Olena', salary: 1300 },
+      ],
+      promotion: [
+        { name: 'Oleg', salary: 1400 },
+        { name: 'Masha', salary: 1700 },
+      ],
+    },
+    sellers: [
+      { name: 'Max', salary: 900 },
+      { name: 'Donald', salary: 700 },
+      { name: 'Joe', salary: 1100 },
+    ],
+  },
+  designer: [{ name: 'Kate', salary: 1800 }],
+};
+
+function summary() {
+  let sum = 0;
+  return function sumSallary(obj) {
+    for (let key in obj) {
+      if (Array.isArray(obj[key])) {
+        obj[key].forEach((employee) => (sum += employee.salary));
+      } else {
+        sumSallary(obj[key]);
+      }
+    }
+    return sum;
+  };
+}
+
+console.log(summary()(employees));
+
+/*function sumSallary(obj) {
+  let sum = 0;
+  for (let key in obj) {
+    console.log('1 for in', sum);
+    if (Array.isArray(obj[key])) {
+      console.log('2 if', sum);
+      obj[key].forEach((employee) => {
+        console.log('3 forEach', sum);
+        sum += employee.salary;
+      });
+    } else {
+      console.log('4 else before', sum);
+      sumSallary(obj[key]);
+      console.log('5 else after', sum);
     }
   }
   return sum;
-}
+}*/
 
-function deepCount2(array) {
-  let sum = array.length;
-  for (const el of array) {
-    Array.isArray(el) ? (sum += deepCount1(el)) : null;
-  }
-  return sum;
-}
-
-console.log(deepCount1([])); //0
-console.log(deepCount1([1, 2, 3])); //3
-console.log(deepCount1(['x', 'y', ['z']])); //4
-console.log(deepCount1([1, 2, [3, 4, [5]]])); //7
-console.log(deepCount1([[[[]]]])); //3
-
-console.log(deepCount2([])); //0
-console.log(deepCount2([1, 2, 3])); //3
-console.log(deepCount2(['x', 'y', ['z']])); //4
-console.log(deepCount2([1, 2, [3, 4, [5]]])); //7
-console.log(deepCount2([[[[]]]])); //3
-//3.
-
+//console.log(sumSallary(employees));
 //4.
 
 //5.
